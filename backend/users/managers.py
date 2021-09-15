fd37294584f48c14ca0a7d330ca92b9377312e17
Sagar Rana <sagarrana0297@gmail.com>
2021-09-15 18:24:39 +0530

    add users model and manager

---
+++ backend/users/managers.py
@@ -0,0 +1,44 @@
+# !/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 15 2021
+@author: sagrana
+"""
+
+import uuid
+from django.db import models
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
+from django.utils.translation import gettext as _
+
+
+class UserManager(BaseUserManager):
+    """
+    creating a manager for a custom user model
+    """
+    def create_user(self, email, role, password=None):
+        """
+        Create and return a `User` with an email, role and password.
+        """
+        if not email:
+            raise ValueError(_('Users Must Have an email address'))
+
+        user = self.model(
+            email=self.normalize_email(email),
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, role, password):
+        """
+        Create and return a `User` with superuser (admin) permissions.
+        """
+        if password is None:
+            raise TypeError(_('Superusers must have a password.'))
+
+        user = self.create_user(email, password)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save()
+
+        return user
--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,55 @@
+# !/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 15 2021
+@author: sagrana
+"""
+import uuid
+from django.contrib.auth.base_user import AbstractBaseUser
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+from .managers import UserManager
+
+
+class User(AbstractBaseUser):
+    """
+        creating a custom user model
+    """
+    # These fields tie to the roles!
+    ADMIN = 1
+    MANAGER = 2
+    EMPLOYEE = 3
+
+    ROLE_CHOICES = (
+        (ADMIN, 'Admin'),
+        (MANAGER, 'Manager'),
+        (EMPLOYEE, 'Employee')
+    )
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    email = models.EmailField(
+        verbose_name='email address',
+        max_length=255,
+        unique=True
+        )
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    created_date = models.DateTimeField(default=timezone.now)
+    modified_date = models.DateTimeField(default=timezone.now)
+    role = models.PositiveSmallIntegerField(choices=ROLE_CHOICES, blank=True, null=True, default=7)
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    # Tells Django that the UserManager class defined above should manage
+    # objects of this type.
+    objects = UserManager()
+
+    def __str__(self):
+        return self.email
+
+    class Meta:
+        """
+        to set table name in database
+        """
+        db_table = "user"
