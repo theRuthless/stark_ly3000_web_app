53949b3f9984695959bdb628e10f4ea4210b4418
Sagar Rana <sagarrana0297@gmail.com>
2021-09-15 23:01:31 +0530

    add user views

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,3 +1,59 @@
-from django.shortcuts import render
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 15 2021
+@author: sagrana
+"""
+from rest_framework import status
+from rest_framework.generics import CreateAPIView, RetrieveAPIView
+from rest_framework.response import Response
+from rest_framework.permissions import AllowAny
+from .models import User
+from .serializers import UserRegistrationSerializer
+from .serializers import UserLoginSerializer
 
-# Create your views here.
+
+class UserRegistrationView(CreateAPIView):
+    """"UserRegistrationView
+    """
+    serializer_class = UserRegistrationSerializer
+    permission_classes = (AllowAny,)
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        response = {
+            'success': 'True',
+            'status code': status.HTTP_200_OK,
+            'message': 'User registered  successfully',
+            }
+        status_code = status.HTTP_200_OK
+        return Response(response, status=status_code)
+
+
+class UserLoginView(RetrieveAPIView):
+    """UserLoginView
+    """
+
+    permission_classes = (AllowAny,)
+    serializer_class = UserLoginSerializer
+    queryset = User.objects.all()
+
+    def post(self, request):
+        """post
+
+        :param request:
+        :return:
+        """
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        response = {
+            'success': 'True',
+            'status code': status.HTTP_200_OK,
+            'message': 'User logged in  successfully',
+            'token': serializer.data['token'],
+            }
+        status_code = status.HTTP_200_OK
+
+        return Response(response, status=status_code)
