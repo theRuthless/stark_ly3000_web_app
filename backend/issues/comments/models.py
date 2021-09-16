70630d86bddbb59f0a7d088d35c9e57d7a976e12
Sagar Rana <sagarrana0297@gmail.com>
2021-09-16 16:00:43 +0530

    comments app

--- backend/issues/comments/models.py
+++ backend/issues/comments/models.py
@@ -1,3 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 16 2021
+@author: sagrana
+"""
+import uuid
+from django.utils.translation import gettext as _
 from django.db import models
+from django.db.models import Manager
 
-# Create your models here.
+
+class Comment(models.Model):
+    """Comment Model
+    """
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    value = models.CharField(max_length=300)
+    owner = models.ForeignKey("users.User", on_delete=models.CASCADE)
+    created_at = models.DateTimeField(_("Date Created"), auto_now_add=True)
+
+    objects = Manager()
+
+    def __str__(self):
+        return self.value
+
+    class Meta:
+        """
+        to set table name in database
+        """
+        db_table = "comment"
--- backend/issues/comments/views.py
+++ backend/issues/comments/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_jwt.authentication import JSONWebTokenAuthentication
 
-# Create your views here.
+from .serializers import CommentSerializer
+from .models import Comment
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    serializer_class = CommentSerializer
+    queryset = Comment.objects.all()
+    permission_classes = (IsAuthenticated,)
+    authentication_class = JSONWebTokenAuthentication
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
