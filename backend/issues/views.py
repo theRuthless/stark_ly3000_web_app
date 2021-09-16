3b5f2a5601425fd1b5c29efa65c5fe860248a6b9
Sagar Rana <sagarrana0297@gmail.com>
2021-09-16 21:53:04 +0530

    issue app views update

--- backend/issues/views.py
+++ backend/issues/views.py
@@ -14,17 +14,44 @@ from .models import Issue
 
 
 class IssueListCreateAPIView(ListCreateAPIView):
+    """IssueListCreateAPIView
+    """
     permission_classes = (IsAuthenticated,)
     authentication_class = JSONWebTokenAuthentication
     serializer_class = IssueSerializer
 
     def get_queryset(self):
+        """
+                Get the list of items for this view.
+                This must be an iterable, and may be a queryset.
+                Defaults to using `self.queryset`.
+
+                This method should always be used rather than accessing `self.queryset`
+                directly, as `self.queryset` gets evaluated only once, and those results
+                are cached for all subsequent requests.
+
+                You may want to override this if you need to provide different
+                querysets depending on the incoming request.
+
+                (Eg. return a list of items that is specific to the user)
+        """
+
         return Issue.objects.all()
 
     def perform_create(self, serializer):
+        """perform_create
+        :param serializer:
+        :return:
+        """
         serializer.save(reporter=self.request.user)
 
     def create(self, request, *args, **kwargs):
+        """create
+        :param request:
+        :param args:
+        :param kwargs:
+        :return:
+        """
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         self.perform_create(serializer)
@@ -33,7 +60,18 @@ class IssueListCreateAPIView(ListCreateAPIView):
 
 
 class IssueDetailAPIView(RetrieveUpdateDestroyAPIView):
+    """IssueDetailAPIView
+    """
     serializer_class = IssueSerializer
     queryset = Issue.objects.all()
     permission_classes = (IsAuthenticated,)
     authentication_class = JSONWebTokenAuthentication
+
+    def patch(self, request, *args, **kwargs):
+        """patch
+        :param request:
+        :param args:
+        :param kwargs:
+        :return:
+        """
+        pass
