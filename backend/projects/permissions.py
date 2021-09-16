592e74fc47081325efc7f1f57c94e3ebc6f5aebc
Sagar Rana <sagarrana0297@gmail.com>
2021-09-16 21:59:10 +0530

    project permissions

---
+++ backend/projects/permissions.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 15 2021
+@author: sagrana
+"""
+
+
+def has_admin_permission(request):
+    return request.user.role == 1 or request.user.is_superuser
