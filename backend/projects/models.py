0b388002fedefbeaeede4996bb027aad3be12949
Sagar Rana <sagarrana0297@gmail.com>
2021-09-16 21:55:52 +0530

    project model

--- backend/projects/models.py
+++ backend/projects/models.py
@@ -1,3 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 16 2021
+@author: sagrana
+"""
+import uuid
 from django.db import models
 
+
 # Create your models here.
+class Project(models.Model):
+    """Project ORM model
+    """
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    title = models.CharField(max_length=30, unique=True)
+    description = models.CharField(max_length=300)
+    owner = models.ForeignKey("users.User", on_delete=models.CASCADE)
+    objects = models.Manager()
+
+    class Meta:
+        db_table = "projects"
