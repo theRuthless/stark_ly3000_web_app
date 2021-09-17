1ec14f5d8e9dbb7c858675031d8a2b206eeca83f
Sagar Rana <sagarrana0297@gmail.com>
2021-09-17 13:55:45 +0530

    email service in watcher

--- backend/watchers/utils.py
+++ backend/watchers/utils.py
@@ -4,8 +4,20 @@
 Created on Wed Sep 17 2021
 @author: sagrana
 """
+import smtplib, ssl
+
+port = 465  # For SSL
+smtp_server = "smtp.gmail.com"
+sender_email = "my@gmail.com"  # Enter your address
+receiver_email = "your@gmail.com"  # Enter receiver address
+password = "testt"
 
 
 def notify_user(watcher_id):
     # Fetch information for Email Body using watcher id
     print("Email Sent!!")
+    message = "Watcher Added"
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+        server.login(sender_email, password)
+        server.sendmail(sender_email, receiver_email, message)
