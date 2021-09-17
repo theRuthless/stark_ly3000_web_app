207c2eeea7e6b24d1a30e523667d1163f2f8828a
Sagar Rana <sagarrana0297@gmail.com>
2021-09-17 13:33:19 +0530

    watchers utils email

---
+++ backend/watchers/utils.py
@@ -0,0 +1,11 @@
+# !/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 17 2021
+@author: sagrana
+"""
+
+
+def notify_user(watcher_id):
+    # Fetch information for Email Body using watcher id
+    print("Email Sent!!")
